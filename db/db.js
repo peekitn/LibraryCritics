e4745978c238a45c73f0bc146341be4cd57ea01a
peekitn <franciscoafbatalha@hotmail.com>
2026-02-11 18:21:38 -0300

    try

--- db/db.js
+++ db/db.js
@@ -1,12 +1,23 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME
-});
+let pool;
+
+if (process.env.DATABASE_URL) {
+  // PRODUÇÃO (Render)
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+  });
+} else {
+  // LOCAL
+  pool = new Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME
+  });
+}
 
 module.exports = pool;
