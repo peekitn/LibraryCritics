38571173d51bbf874054ecbf4a310b3c5c55213b
peekitn <franciscoafbatalha@hotmail.com>
2026-02-12 09:27:09 -0300

    corrr

--- db/db.js
+++ db/db.js
@@ -5,8 +5,11 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME
+  port: parseInt(process.env.DB_PORT) || 5432, // garante que seja número
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false // importante para alguns bancos na nuvem
+  }
 });
 
 module.exports = pool;
